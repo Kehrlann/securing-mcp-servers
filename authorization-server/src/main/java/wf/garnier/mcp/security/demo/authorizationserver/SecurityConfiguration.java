e940c5187a5673cfaba412daf001e4619ca81fee
Daniel Garnier-Moiroux <git@garnier.wf>
2025-10-07 00:50:28 +0200

    demo: authorization server now has users with email

--- authorization-server/src/main/java/wf/garnier/mcp/security/demo/authorizationserver/SecurityConfiguration.java
+++ authorization-server/src/main/java/wf/garnier/mcp/security/demo/authorizationserver/SecurityConfiguration.java
@@ -2,10 +2,18 @@ package wf.garnier.mcp.security.demo.authorizationserver;
 
 import java.util.List;
 
+import wf.garnier.mcp.security.demo.authorizationserver.user.DemoUser;
+import wf.garnier.mcp.security.demo.authorizationserver.user.DemoUserDetailsService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import static org.springaicommunity.mcp.security.authorizationserver.config.McpAuthorizationServerConfigurer.mcpAuthorizationServer;
@@ -15,25 +23,43 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 class SecurityConfiguration {
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
-                .with(mcpAuthorizationServer(), withDefaults())
-                .formLogin(withDefaults())
-                .cors(cors -> cors.configurationSource(_ -> configurationSource()))
-                .build();
-    }
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+			.with(mcpAuthorizationServer(), withDefaults())
+			.formLogin(withDefaults())
+			.cors(cors -> cors.configurationSource(_ -> configurationSource()))
+			.build();
+	}
 
-    private CorsConfiguration configurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new DemoUserDetailsService(new DemoUser("daniel", "pw", "daniel@example.com"),
+				new DemoUser("alice", "pw", "alice@example.com"), new DemoUser("bob", "pw", "bob@example.com"));
+	}
 
-        return configuration;
+	@Bean
+	OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return ctx -> {
+			DemoUser user = ctx.getPrincipal();
+			ctx.getClaims().subject(user.getUserEmail());
+			if (ctx.getTokenType().getValue().equals(OidcParameterNames.ID_TOKEN)) {
+				ctx.getClaims().claim(StandardClaimNames.EMAIL, user.getUserEmail());
+				ctx.getClaims().claim(StandardClaimNames.EMAIL_VERIFIED, true);
+                ctx.getClaims().claim(StandardClaimNames.NAME, user.getUsername());
+			}
+		};
+	}
 
-    }
+	private CorsConfiguration configurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
+
+		return configuration;
+
+	}
 
 }
